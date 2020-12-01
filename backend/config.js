d206ebcb336fc125852c370e88b37be0549bb174
adithya <adithyanuzpratamaputra@gmail.com>
2020-12-01 19:07:43 +0700

    upload on heroku

--- backend/config.js
+++ backend/config.js
@@ -4,5 +4,6 @@ dotenv.config();
 
 export default{
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/tokopaedi',
-    JWT_SECRET: process.env.JWT_SECRET || 'somthingsecret'
+    JWT_SECRET: process.env.JWT_SECRET || 'somthingsecret',
+    PORT: process.env.PORT || 5000
 }
