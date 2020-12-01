321c95c3792f106a8a1729d4b3aa01045813b29d
adithya <adithyanuzpratamaputra@gmail.com>
2020-12-01 20:04:35 +0700

    upload on heroku

--- backend/server.js
+++ backend/server.js
@@ -50,5 +50,7 @@ app.get('*', (req, res) =>
 app.use((err, req, res, next) => {
     res.status(500).send({message: err.message})
 })
-
-app.listen(5000, () => {console.log("Server started at http://localhost:5000")});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
+  });
